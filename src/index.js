02023f12c4300dd02dd58a463722ec5226148f06
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-28 22:13:26 +0100

    Added try-catchs statement to handle errors in index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import Transaction from "./transaction.js";
 
 const testAccount = new Account();
 const Ble = new Client("Ble", testAccount);
+try {
 Ble.newTransaction(Transaction, "1276", "deposit", 1000, (new Date (2012, 0, 10)));
 Ble.newTransaction(Transaction, "3478", "deposit", 2000, (new Date (2012, 0, 13)));
 Ble.newTransaction(Transaction, "8756", "withdraw", 500, (new Date (2012, 0, 14)));
 Ble.printStatement(Printer);
+} catch (error) {
+    console.log(`An error has occured. Reason: ${error.message}`)
+}
