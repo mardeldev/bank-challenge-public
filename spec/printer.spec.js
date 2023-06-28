63bf89b482c195ff1cdf51ffba585a0451e1caf6
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-28 20:00:33 +0100

    Test 3b: Written a failing test

---
+++ spec/printer.spec.js
@@ -0,0 +1,55 @@
+import Printer from "../src/printer.js"
+describe('User Story 3 Test:', () => {
+    let testClient;
+    let testAccount;
+    let clgSpy;
+    class mockTransaction {
+        constructor(id, transactionType, amount, transactionDate){
+            this.id = id;
+            this.transactionType = transactionType;
+            this.amount = amount;
+            this.transactionDate = transactionDate;
+        }
+    }
+
+    class mockClient {
+    
+        constructor(name, account){
+            this.name = name;
+            this.account = account;
+        }
+        newTransaction(Transaction, id, transactionType, amount, transactionDate){
+            this.transaction = new Transaction(id, transactionType, amount, transactionDate);
+            this.addTransactionToAccount(this.transaction);
+        }
+        addTransactionToAccount(){ this.account.addTransactionToAccount(this.transaction); }
+    }
+
+    class mockAccount {
+        transactionHistory = [];
+        accountNumber = 1;
+        balance = 0;
+        addTransactionToAccount(transactionToAdd){}
+    }
+
+    beforeEach(() => {
+        testAccount = new mockAccount();
+        testClient = new mockClient(`Ble`, testAccount);
+        clgSpy = spyOn(console, "log");
+        
+    })
+
+    afterEach(() => {
+        testAccount = undefined;
+    })
+
+    it('3b. should call on console log', () => {
+        // Arrange -> testAccount and testClient instantiation done in beforeEach.
+        testClient.newTransaction(mockTransaction, '001', 'deposit', 100, '01/01/01');
+        // Act
+        testClient.printStatement(Printer);
+        // Assert
+        expect(clgSpy).toHaveBeenCalled();
+    })
+
+})
