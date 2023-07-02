c7658e0c92bb0cdb7d154bcd69c3a0fd63adf8a6
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-07-02 18:29:42 +0100

    Refactored transaction.js to auto generate transaction id

--- src/transaction.js
+++ src/transaction.js
@@ -1,11 +1,20 @@
 class Transaction{
-    constructor(id, transactionType, amount, transactionDate){
-        this.id = id;
+    idHistory = [];
+    constructor(transactionType, amount, transactionDate){
+        this.id = Transaction.transactionIdGenerator;
         this.transactionType = transactionType;
         this.amount = amount;
         const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
         this.transactionDate = transactionDate.toLocaleDateString('en-GB', options);
     }
+
+    static transactionIdGenerator(){
+        while(!idHistory.includes(newId)){
+            newId = Math.floor(Math.random() * 10000) + 1000;
+            idHistory.push(newId)
+        }
+    }
+        
 }
 
 export default Transaction;
